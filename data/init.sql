1cce85e3c18f341e5a6d0957a13ae403c0e5ccb1
Krzysztof <k.sokalski@onet.pl>
2024-12-25 14:29:25 +0100

    -modify user in the databse to hold password reset token, create functions to find user by reset token and email, create function to update reset token, create api post method to request password reset and create the token, and send an email to the user, create api get method to verify the token and redirect the user to frontend site, create api post method to reset the password when user provides the token and new password, create EmailUtil class to handle email call, add new dependency in pom.xml to handle email calls, modify init.sql to create the db with new fields

--- data/init.sql
+++ data/init.sql
@@ -6,9 +6,11 @@ USE betterfb;
 CREATE TABLE IF NOT EXISTS USER (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(100) NOT NULL,
-    password varchar(100) NOT NULL,
+    password VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    resettoken VARCHAR(255),  -- Token for resetting password
+    resettokenexpiration TIMESTAMP  -- Expiration time for the reset token
 );
 
 -- Add any other tables or initialization logic here
